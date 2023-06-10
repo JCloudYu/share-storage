af2c5b5a9043cc91fd7fcc0771b0a67e57a6f24a
Cloud Yu <j.cloud.yu@gmail.com>
2023-06-10 23:56:05 +0800

    Publish shared-storage as shstore library

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 interface StorageClear {(identifier:string|object, clear:true):void}
 declare global {
-	interface ExtendedSharedStorage {(identifier:string):{[key:string|number|symbol]:any}}
+	interface SStorageExt {(identifier:string):{[key:string|number|symbol]:any}}
+	type ExtSharedStorage = SStorageExt;
 }
 
 interface SharedStorage extends StorageClear, ExtendedSharedStorage {}
